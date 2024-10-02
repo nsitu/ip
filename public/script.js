8bcd40d3cae70370ea234f471a45c56a3b8f1ab5
Harold Sikkema <harold@nsitu.ca>
2024-10-02 10:17:50 -0400

    Delete public directory

--- public/script.js
+++
@@ -1,9 +0,0 @@
-// fetch the datetime from API endpoint on NodeJS and populate the content div
-const getData = async () =>{
-    console.log('Hello from script.js')
-    const response = await fetch('/api/datetime')
-    const data = await response.json()
-    document.querySelector('#content').innerHTML = data.datetime
-}
-
-getData()
