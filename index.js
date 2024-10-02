cda1fb71b6eba57d2186b8cde5e8c3d0a875a8eb
Harold Sikkema <harold@nsitu.ca>
2024-10-02 09:37:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.use('/',express.static('./public')) 
 
+
+app.get("/ip", (req, res) => { 
+    res.send(req) 
+}); 
+
 app.get("/api/datetime", (req, res) => { 
     res.send({
         datetime: new Date()
